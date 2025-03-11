23fae5c9f904b273336cde7ae60529b94558f35d
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-03-12 00:57:49 +0900

    Data-Visualization - SAVE THE PANDAS(Global Time Series: .drop())

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ totals_df = (
     daily_df[["Confirmed", "Deaths", "Recovered"]].sum().reset_index(name="count")
 )
 totals_df = totals_df.rename(columns={"index": "condition"})
+
+countries_df = daily_df[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
+countries_df = countries_df.groupby("Country_Region").sum().reset_index()
