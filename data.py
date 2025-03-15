cff347a50d347aeca3313a93e5b0604701c8f9d0
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-03-15 22:38:00 +0900

    Data-Visualization - DASHBOARD(Introduction)

---
+++ data.py
@@ -0,0 +1,67 @@
+# Data
+
+import pandas as pd
+
+conditions = ["confirmed", "deaths", "recovered"]
+
+daily_df = pd.read_csv("data/daily_report.csv")
+
+# Total
+totals_df = (
+    daily_df[["Confirmed", "Deaths", "Recovered"]].sum().reset_index(name="count")
+)
+totals_df = totals_df.rename(columns={"index": "condition"})
+
+# Country Total
+countries_df = daily_df[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
+countries_df = countries_df.groupby("Country_Region").sum().reset_index()
+
+
+# Country - Day
+def make_country_df(country):
+    def make_df(condition):
+        df = pd.read_csv(f"data/time_{condition}.csv")
+        df = df.loc[df["Country/Region"] == country]
+        df = (
+            df.drop(columns=["Province/State", "Country/Region", "Lat", "Long"])
+            .sum()
+            .reset_index(name=condition)
+        )
+        df = df.rename(columns={"index": "date"})
+        return df
+
+    final_df = None
+
+    for condition in conditions:
+        condition_df = make_df(condition)
+        if final_df is None:
+            final_df = condition_df
+        else:
+            final_df = final_df.merge(condition_df)
+    return final_df
+
+
+# Global - Day
+def make_global_df():
+    def make_df(condition):
+        df = pd.read_csv(f"data/time_{condition}.csv")
+        df = (
+            df.drop(["Province/State", "Country/Region", "Lat", "Long"], axis=1)
+            .sum()
+            .reset_index(name=condition)
+        )
+        df = df.rename(columns={"index": "date"})
+        return df
+
+    final_df = None
+
+    for condition in conditions:
+        condition_df = make_df(condition)
+        # 처음에만 실행됨
+        if final_df is None:
+            final_df = condition_df
+        else:
+            # .merge()를 사용하면 공통키를 기준으로 병합해줌
+            final_df = final_df.merge(condition_df)
+
+    return final_df
--- main.py
+++ main.py
@@ -1,37 +1,33 @@
+# Data Visualization
+from dash import Dash, html, dcc
+import plotly.express as px
 import pandas as pd
 
+app = Dash()
 
-def make_global_df(condition):
-    df = pd.read_csv(f"data/time_{condition}.csv")
-    df = (
-        df.drop(["Province/State", "Country/Region", "Lat", "Long"], axis=1)
-        .sum()
-        .reset_index(name=condition)
-    )
-    df = df.rename(columns={"index": "date"})
-    return df
-
-
-daily_df = pd.read_csv("data/daily_report.csv")
-
-totals_df = (
-    daily_df[["Confirmed", "Deaths", "Recovered"]].sum().reset_index(name="count")
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame(
+    {
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
+    }
 )
-totals_df = totals_df.rename(columns={"index": "condition"})
 
-countries_df = daily_df[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
-countries_df = countries_df.groupby("Country_Region").sum().reset_index()
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
+app.layout = html.Div(
+    children=[
+        html.H1(children="Hello Awesome"),
+        html.Div(
+            children="""
+        Dash: A web application framework for your data.
+    """
+        ),
+        dcc.Graph(id="example-graph", figure=fig),
+    ]
+)
 
-conditions = ["confirmed", "deaths", "recovered"]
-
-final_df = None
-
-for condition in conditions:
-    condition_df = make_global_df(condition)
-    # 처음에만 실행됨
-    if final_df is None:
-        final_df = condition_df
-    else:
-        # .merge()를 사용하면 공통키를 기준으로 병합해줌
-        final_df = final_df.merge(condition_df)
+if __name__ == "__main__":
+    app.run(debug=True)
