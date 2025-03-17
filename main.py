4dc4b6322353252ca61722a23ae3912bebfbaee9
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-03-17 17:23:49 +0900

    Data Visualization - DASHBOARD(Bars Figure)

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 # Data Visualization
 from dash import Dash, html, dcc
 import plotly.express as px
-from data import countries_df
+from data import countries_df, totals_df
 from table_builders import make_table
 
-# .columns를 이용하여 DataFrame의 헤더 제목을 가져올 수 있음
-# print(countries_df.columns)
-
-# .values를 이용하여 DataFrame의 데이터셀을 가져올 수 있음
-# print(countries_df.values)
 
 stylesheets = [
     "https://cdn.jsdelivr.net/npm/reset-css@5.0.2/reset.min.css",
@@ -17,13 +12,16 @@ stylesheets = [
 
 app = Dash(external_stylesheets=stylesheets)
 
+# Plotly
 bubble_map = px.scatter_geo(
     countries_df,
     locations="Country_Region",
     locationmode="country names",
     template="plotly_dark",
     projection="natural earth",
+    color_continuous_scale=px.colors.sequential.Oryel,
     color="Confirmed",
+    title="Confirmed by Country",
     size="Confirmed",
     size_max=30,
     hover_name="Country_Region",
@@ -35,11 +33,20 @@ bubble_map = px.scatter_geo(
         "Country_Region": False,
     },
 )
+bubble_map.update_layout(margin=dict(l=0, r=0, t=50, b=0))
 
+bars_graph = px.bar(
+    totals_df,
+    x="condition",
+    y="count",
+    hover_data={"count": ":,"},
+    template="plotly_dark",
+    title="Total Global Cases",
+)
+bars_graph.update_layout(xaxis=dict(title="Condition"), yaxis=dict(title="Count"))
+
+# Dash!!!
 app.layout = html.Div(
-    # html.H1("Corona Dashboard"),
-    # 위와 같이 사용해도 되지만 알아보기 힘듦.
-    # children을 명시해주고 배열 리스트로 넣어주는 것이 좋음.
     style={
         "minHeight": "100vh",
         "backgroundColor": "#111111",
@@ -57,6 +64,11 @@ app.layout = html.Div(
             children=[
                 html.Div(children=[dcc.Graph(figure=bubble_map)]),
                 html.Div(children=[make_table(countries_df)]),
+            ]
+        ),
+        html.Div(
+            children=[
+                html.Div(children=[dcc.Graph(figure=bars_graph)]),
             ]
         ),
     ],
